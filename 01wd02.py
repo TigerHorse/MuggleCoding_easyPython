3d7f1d3cbb5b0030e88ac793c3d0ca2bcd614dce
fengdasuk19 <fengdasuk19@gmail.com>
2016-05-14 12:42:49 +0800

    add 01w02.py

---
+++ 01wd02.py
@@ -0,0 +1,37 @@
+from bs4 import BeautifulSoup
+
+finPath = "../../../Plan-for-combating-master/week1/1_2/\
+1_2answer_of_homework/\
+1_2_homework_required/index.html"
+
+# Read and Analyse
+with open(finPath, 'r') as fin:
+    Soup = BeautifulSoup(fin.read(), 'lxml')
+    itemTitle = Soup.select('body > div.container > div.row > div.col-md-9 > \
+div.row > div > div > div.caption > h4:nth-of-type(2) > a')
+    itemPicSrc = Soup.select('body > div.container > div.row > div.col-md-9 > \
+div.row > div > div > img')
+    itemPrice = Soup.select('body > div.container > div.row > div.col-md-9 > \
+div.row > div > div > div.caption > h4.pull-right')
+    itemComment = Soup.select('body > div.container > div.row > div.col-md-9 > \
+div.row > div > div > div.ratings > p.pull-right')
+    itemStar = Soup.select('body > div.container > div.row > div.col-md-9 > \
+div.row > div > div > div.ratings > p:nth-of-type(2)')
+
+
+# Save Result
+dataSet = []
+
+for iTitle, iPic, iPrice, iComment, iStar in zip(itemTitle, itemPicSrc, itemPrice, itemComment, itemStar):
+    info = {
+        'iTitle': iTitle.get_text(),
+        'iPic': iPic.get('src'),
+        'iPrice': iPrice.get_text(),
+        'iComment': iComment.get_text(),
+        'iStar': len(iStar.find_all("span", class_="glyphicon glyphicon-star"))
+    }
+    dataSet.append(info)
+
+# See Result
+for info in dataSet:
+    print(info)
