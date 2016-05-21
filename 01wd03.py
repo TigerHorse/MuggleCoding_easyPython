b7afc6345803aa49010b03a3d87eb32801996ec2
fengdasuk19 <fengdasuk19@gmail.com>
2016-05-21 14:01:29 +0800

    modify 01wd03.py(recognization of gender)

--- 01wd03.py
+++ 01wd03.py
@@ -2,12 +2,17 @@
 from bs4 import BeautifulSoup as bs
 import requests
 
+# CONSTANTS
+FEMALE_TIP = '\xe5\xa5\xb9'
+GENDER_TIP = u'\u6027\u522b\uff1a'
+FEMALE = '\xe5\xa5\xb3'
+MALE = '\xe7\x94\xb7'
+
 # Source
 urlFather = 'http://bj.xiaozhu.com/search-duanzufang-p1-0/'
 webDataFather = requests.get(urlFather)
 soupFather = bs(webDataFather.text, 'lxml')
 url = (soupFather.select('#page_list > ul > li > a'))[0].get('href')
-
 webData = requests.get(url)
 soup = bs(webData.text, 'lxml')
 
@@ -16,22 +21,28 @@ title = [i.get_text() for i in soup.select('div.pho_info > h4 > em')][0]
 address = [i.get_text() for i in soup.select('body > div.wrap.clearfix.con_bg > div.con_l > div.pho_info > p > span.pr5')][0]
 feed_pd = [i.get_text() for i in soup.select('#pricePart > div.day_l > span')][0]
 imgHouse001 = [i.get('src') for i in soup.select('#curBigImage')][0]
+
 nameLandlord = [i.get('title') for i in soup.select('#floatRightBox > div.js_box.clearfix > div.w_240 > h6 > a')][0]
 imgLandlord = [i.get('src') for i in soup.select('#floatRightBox > div.js_box.clearfix > div.member_pic > a > img')][0]
-
 profileURL = [i.get('href') for i in soup.select('#floatRightBox > div.js_box.clearfix > div.w_240 > h6 > a')][0]
 profileGet = requests.get(profileURL)
 profileSoup = bs(profileGet.text, 'lxml')
-if profileSoup.find_all("dl", class_="con_404_1 paddingB13"):
-    gender = u'\u672a\u77e5'
+
+if 0 == len(profileSoup.find_all("ul", class_="fd_person")):
+    genderStatement = [i.get_text() for i in profileSoup.select('body > div.contentFD.clearfix > div.fd_con > \
+div.fd_infor > dl > dt')][0]
+    if FEMALE_TIP == genderStatement[0].encode('utf-8'):
+        gender = FEMALE
+    else:
+        gender = MALE
 else:
-    gender = [i.get_text() for i in profileSoup.select('ul.fd_person > li:nth-of-type(1)')][0].strip(u'\u6027\u522b\uff1a')
+    gender = [i.get_text() for i in profileSoup.select('ul.fd_person > li:nth-of-type(1)')][0].strip(GENDER_TIP).encode('utf-8')
 
 print('[Title] {}\n[Address] {}\n[Price] {}\n[Image URL(download it and open in Chrome)] {}'.format(title.encode('utf-8'), address.encode('utf-8'),
                                                                                                     feed_pd, imgHouse001))
 print('[Information about the landlord]')
 print('    [Name  ] {}'.format(nameLandlord.encode('utf-8')))
-print('    [Gender] {}'.format(gender.encode('utf-8')))
+print('    [Gender] {}'.format(gender))
 print('    [Image ] {}'.format(imgLandlord))
 
 
@@ -46,14 +57,6 @@ webDataFather = requests.get(urlFather)
 soupFather = bs(webDataFather.text, 'lxml')
 linkList = soupFather.select('#page_list > ul > li > a')
 
-# for each page, you should get the following things:
-#   title
-#   address
-#   fee_pd
-#   imgHouse001
-#   landlordImg
-#   landlordGender
-#   landlordName
 
 title = []
 
